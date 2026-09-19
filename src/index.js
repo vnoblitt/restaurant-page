589bbaac3a7c04d64742660f528ebeee0cde127e
v <261508724+vnoblitt@users.noreply.github.com>
2026-09-19 14:26:57 -0500

    Laid out the very basics of the homepage before getting overwhelmed and deciding to call it for now.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import "./styles.css";
+/* import pizza from "./pizza.jpg";
+
+const image = document.createElement("img");
+image.src = pizza;
+image.width = 500;
+
+document.body.appendChild(image); */
+
+console.log('hello?');
