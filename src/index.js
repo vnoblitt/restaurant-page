9c2c794948404a9fd002bf882ea314fc4c02dc8c
v <261508724+vnoblitt@users.noreply.github.com>
2026-09-22 20:53:12 -0500

    Messed with CSS, I'm gonna work for another day then call it good regardless. This stinks.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 // src/index.js
 import "./styles.css";
+import {homeTab} from "./home.js";
+import { menuTab } from "./menu.js";
+import { aboutTab } from "./about.js";
 /* import pizza from "./pizza.jpg";
 
 const image = document.createElement("img");
 image.src = pizza;
 image.width = 500;
 
-document.body.appendChild(image); */
+document.body.appendChild(image); 
+*/
+const homeButton = document.getElementById('home');
+const menuButton = document.getElementById('menu');
+const aboutButton = document.getElementById('about');
 
+
+const content = document.getElementById('content');
+content.append(homeTab);
 console.log('hello?');
+
+homeButton.addEventListener('click', () => {
+    content.innerHTML = ``;
+    content.append(homeTab);
+});
+
+menuButton.addEventListener('click', () => {
+    content.innerHTML = ``;
+    content.append(menuTab);
+});
+
+aboutButton.addEventListener('click', () => {
+    content.innerHTML = ``;
+    content.append(aboutTab);
+});
